001206745f1a12832374b90696c6b4fad998fc88
Joe <j.p.de.lange88@gmail.com>
2019-02-11 20:27:35 +0200

    stats

---
+++ View_Models/AllStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AGS.ServerAPI.View_Models
+{
+    public class AllStats
+    {
+        public List<clsClients> Clients { get; set; }
+        public List<clsUsers> Users { get; set; }
+        public List<clsUserExperience> Uxs { get; set; }
+        public List<clsTechExperience> Txs { get; set; }
+        public List<clsExperienceAnswers> Xans { get; set; }
+        public List<clsResult> Results { get; set; }
+    }
+}
